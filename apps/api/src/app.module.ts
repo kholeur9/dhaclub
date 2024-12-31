54d1f4bb6ea7bef453911505d04725ba4d7f99b6
kholeur9 <65097234+kholeur9@users.noreply.github.com>
2024-12-31 18:07:23 +0000

    Add auth microservice

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     DrizzleModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
